b9f4a3733a303b43d3a39a27b53456daed19ce90
James Best <jim@justjibba.net>
2022-10-24 15:00:15 +0100

    Add create connection and table functions

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 /*
 Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "github.com/jim-at-jibba/studybuddy/cmd"
+import (
+	"github.com/jim-at-jibba/studybuddy/cmd"
+	"github.com/jim-at-jibba/studybuddy/data"
+)
 
 func main() {
+	data.OpenDatabase()
 	cmd.Execute()
 }
